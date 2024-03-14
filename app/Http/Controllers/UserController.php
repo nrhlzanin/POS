bdb606e82412fb07a3f265c5be452aa5db4276a0
nrhlzanin <anindya.putri202003@gmail.com>
2024-03-14 14:16:03 +0700

    Mengimplementasikan Relationships pada User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,22 +8,28 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index() {
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+    // public function index() {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
+
     public function tambah() {
         return view('user_tambah');
     }
+    public function index() 
+    {
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
+    }
 
-    public function tambah_simpan(Request $request) {
+    public function tambah_simpan(Request $request)
+    {
         UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => Hash::make($request->password),
             'level_id' => $request->level_id
         ]);
-
         return redirect('/user');
     }
 
